13a4529057cb5e5465d7506fefd9d328472ec7d2
Qabotech <81534574+Qabotech@users.noreply.github.com>
2021-09-03 10:34:52 +0200

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -1,1 +1,18 @@
-AOS.init();
+gsap.registerPlugin(ScrollTrigger);
+gsap.to(".Mtxt",{
+    duration:10,
+    scrollTrigger: {
+        trigger:".Mtxt",
+        start:10,
+        toggleClass:"slide-in-bottom",
+    },
+})
+gsap.registerPlugin(ScrollTrigger);
+gsap.to(".con1",{
+    duration:10,
+    scrollTrigger: {
+        trigger:".con1",
+        toggleClass:"slide-in-left",
+    },
+})
+
